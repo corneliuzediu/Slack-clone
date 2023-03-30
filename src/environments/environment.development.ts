a1db56c5431f471e78222bb099e5bf813abff981
corneliuzediu <zediucorneliu@gmail.com>
2023-03-30 08:29:39 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-50162',
+    appId: '1:94348130753:web:638fdb99c9dd0ea7045740',
+    storageBucket: 'slack-clone-50162.appspot.com',
+    apiKey: 'AIzaSyD0roZOZ5NZlx9BLHDlmU505JXJZbATLqA',
+    authDomain: 'slack-clone-50162.firebaseapp.com',
+    messagingSenderId: '94348130753',
+  },};
