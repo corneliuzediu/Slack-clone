fdd59fb935e0f1715a18ab32591373a4d03499e2
corneliuzediu <zediucorneliu@gmail.com>
2023-03-31 07:55:51 +0200

    Comm user class

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -3,13 +3,13 @@ export class User {
     lastName: String;
     email: string;
     telefon: number;
-    ID: string;
-    img: any;
-    title: string;
-    status: string; //Active, AFK, Log out
-    statusTime: string;
-    channels: Array<any>;
-    contacts: Array<any>;
-    searchHistory: Array<any>;
+    ID: string;                     // user ID
+    img: any;                       // img file or path to img
+    title: string;                  // title or extra detail of person
+    status: string;                 // Active, AFK, Log out
+    statusTime: string;             // Active since <data>
+    channels: Array<any>;           // Channels connected to the user
+    contacts: Array<any>;           // Contacts connected to the user
+    searchHistory: Array<any>;      // Search history
 
 }
