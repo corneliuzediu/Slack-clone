4007584700a487a089d8e3a5f3b983e5473bb43d
corneliuzediu <zediucorneliu@gmail.com>
2023-03-31 07:51:37 +0200

    Fixed overlay + User Class

---
+++ src/models/user.class.ts
@@ -0,0 +1,15 @@
+export class User {
+    firstName: string;
+    lastName: String;
+    email: string;
+    telefon: number;
+    ID: string;
+    img: any;
+    title: string;
+    status: string; //Active, AFK, Log out
+    statusTime: string;
+    channels: Array<any>;
+    contacts: Array<any>;
+    searchHistory: Array<any>;
+
+}
