34593a931bfaffc8248606e8b1ba8677c9eb8ab1
DannyKaschub <dannykashub98@gmail.com>
2023-04-09 00:49:24 +0200

    hinzufügen von firebaseAuth und start des einbaus für den login

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -3,6 +3,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserHolderComponent } from '../dialog-add-user-holder/dialog-add-user-holder.component';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { environment } from '../../environments/environment';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,22 +14,34 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   templateUrl: './initial-page.component.html',
   styleUrls: ['./initial-page.component.scss']
 })
+
 export class InitialPageComponent implements OnInit {
   docID = 'P9JqkUnlL2iGANgAKXMm';
+  loginForm: any;
+  formBuilder: any;
+
   constructor(public dialogAddUser: MatDialog, private router: Router, private firestore: AngularFirestore) { }
 
-
   ngOnInit() {
-
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password:['', [Validators.required]]
+    });
   }
 
   bypassLogIn() {
         this.router.navigateByUrl('/main/' + this.docID);
   }
 
-  logIn() {
-    console.log('anmeldung');
+  loginAsUser() {
+
   }
+
+  onSignup() {
+    
+  }
+
+
 
   openDialogAddUser() {
     this.dialogAddUser.open(DialogAddUserHolderComponent)
